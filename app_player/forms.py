cf619dbff0a696d2aafd26a9ba68990297a61ccc
Cyprian <126704996+CyprianJam@users.noreply.github.com>
2023-04-12 13:46:28 +0200

    Initial commit

---
+++ app_player/forms.py
@@ -0,0 +1,29 @@
+from django.forms import CharField, IntegerField, ModelForm
+from app_player.models import Song, Part
+from django import forms
+
+
+class SongForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # for visible in self.visible_fields():
+        #     visible.field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Song
+        fields = ['band', 'song_title', 'song_url', 'description']
+
+        # exclude = ('song_url',)
+
+
+
+class PartForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+
+    class Meta:
+        model = Part
+        fields = '__all__'
